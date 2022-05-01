f17bad732b78442d963263884bd5c235eb72eb52
Mohith Sakthivel <mohithksakthivel@gmail.com>
2022-05-01 18:01:56 -0400

    Fix transforms

--- test_transforms.py
+++ test_transforms.py
@@ -4,64 +4,43 @@ from scipy.spatial.transform import Rotation as R
 from deep_hand_eye.dataset import MVSDataset
 from torch_geometric.loader import DataLoader
 
+import deep_hand_eye.pose_utils as p_utils
+
 dataset = MVSDataset(max_rot_offset=0)
 
 batch_size = 4
 train_loader = DataLoader(dataset, batch_size=batch_size)
 data = next(iter(train_loader))
 
-num_nodes = data[0].edge_attr.shape[0]
-
 
 for n in range(batch_size):
-    for i in range(num_nodes):
-        for j in range(num_nodes):
 
-            if i == j:
-                continue
-            
-            print(" iteration  ", i ,"  ", j)
-            ee_transform = data[n].edge_attr[i][j]
+    hand_eye = data[n].y.squeeze()
+    hand_eye_quat = hand_eye[3:][[1,2,3,0]]
+    hand_eye_homogenous = R.from_quat(hand_eye_quat).as_matrix()
+    hand_eye_translate = np.array(hand_eye[:3]).reshape(-1,1)
+    hand_eye_homogenous = np.hstack([hand_eye_homogenous, hand_eye_translate])
+    hand_eye_homogenous = np.vstack([hand_eye_homogenous, [0,0,0,1]])
 
-            hand_eye = data[n].y
+    for i, (from_idx, to_idx) in enumerate(data[n].edge_index.T):
 
-            cam_transform = data[n].y_edge[i][j]
+        rel_ee_transform = data[n].edge_attr[i]
+        rel_cam_transform = data[n].y_edge[i]
 
-            ee_quat = ee_transform[3:]
-            ee_quat = ee_quat[[1,2,3,0]]
+        ee_quat = rel_ee_transform[3:][[1,2,3,0]]
+        ee_homogenous = R.from_quat(ee_quat).as_matrix()
+        ee_translate = np.array(rel_ee_transform[:3]).reshape(-1,1)
+        ee_homogenous = np.hstack([ee_homogenous, ee_translate])
+        ee_homogenous = np.vstack([ee_homogenous, [0,0,0,1]])
 
-            ee_homogenous = R.from_quat(ee_quat).as_matrix()
+        cam_quat = rel_cam_transform[3:][[1,2,3,0]]
+        cam_homogenous = R.from_quat(cam_quat).as_matrix()
+        cam_translate = np.array(rel_cam_transform[:3]).reshape(-1,1)
+        cam_homogenous = np.hstack([cam_homogenous, cam_translate])
+        cam_homogenous = np.vstack([cam_homogenous, [0,0,0,1]])
 
-            ee_translate = np.array(ee_transform[:3]).reshape(-1,1)
-            
-            ee_homogenous = np.hstack([ee_homogenous, ee_translate])
-
-            ee_homogenous = np.vstack([ee_homogenous, [0,0,0,1]])
-
-            cam_quat = cam_transform[3:]
-            cam_quat = cam_quat[[1,2,3,0]]
-
-            cam_homogenous = R.from_quat(cam_quat).as_matrix()
-            
-            cam_translate = np.array(cam_transform[:3]).reshape(-1,1)
-            
-            cam_homogenous = np.hstack([cam_homogenous, cam_translate])
-
-            cam_homogenous = np.vstack([cam_homogenous, [0,0,0,1]])
-
-            hand_eye_quat = hand_eye[3:]
-            hand_eye_quat = hand_eye_quat[[1,2,3,0]]
-
-            hand_eye_homogenous = R.from_quat(hand_eye_quat).as_matrix()
-            hand_eye_translate = np.array(hand_eye[:3]).reshape(-1,1)
-
-            hand_eye_homogenous = np.hstack([hand_eye_homogenous, hand_eye_translate])
-
-            hand_eye_homogenous = np.vstack([hand_eye_homogenous, [0,0,0,1]])
-
-            print("max error : ", np.max(cam_homogenous - hand_eye_homogenous @ ee_homogenous))
-            print("max element in homogenous matrix : ", np.where(  cam_homogenous - hand_eye_homogenous @ ee_homogenous == np.max(cam_homogenous - hand_eye_homogenous @ ee_homogenous)  )  )
-
-            assert np.all( abs(cam_homogenous - hand_eye_homogenous @ ee_homogenous) < 0.1)
+        err_residuals = cam_homogenous - p_utils.invert_homo(hand_eye_homogenous) @ ee_homogenous @ hand_eye_homogenous
+        print("max error : ", np.max(err_residuals))
+        assert np.all(np.abs(err_residuals) < 1e-5)
 
 
